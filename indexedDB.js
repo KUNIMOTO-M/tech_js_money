b72a16079af3d30c1a584b56746994361eb8705b
KUNIMOTO-M <psmith.mk0203@gmail.com>
2021-08-01 23:05:42 +0900

    データベース接続終了

--- indexedDB.js
+++ indexedDB.js
@@ -1,1 +1,22 @@
 //第4章：収支のデータをデータベースに記録
+const dbName = "kakeiboDB";
+const storeName = "kakeiboStore";
+const dbVersion = 1;
+
+let database = indexedDB.open(dbName, dbVersion);
+
+database.onupgradeneeded = function (event) {
+  let db = event.target.result;
+  db.createObjectStore(storeName, { keyPath: "id"});
+  console.log("データベースを新規作成しました")
+}
+
+database.onsuccess = function (event) {
+  let db = event.target.result;
+  // 接続を解除する
+  db.close();
+  console.log("データベースに接続できました");
+}
+database.onerror = function (event) {
+  console.log("データベースに接続できませんでした");
+}
